9bde678b02dbf14525122b8dc08db62972863e69
Юлия <118264321+Morozova91@users.noreply.github.com>
2024-12-03 21:58:30 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,15 +1,16 @@
 # Однокоренные
 
 def single_root_words(root_word, *other_words):
+    new_root_word = root_word.lower()
     same_words = []
-    root_word = root_word.lower()
     for word in other_words:
-        if root_word in word.lower():
+        word_lower = word.lower()
+        if new_root_word in word_lower or word_lower in new_root_word:
             same_words.append(word)
     return same_words
-
-result1 = single_root_words('dem', 'dEmon', 'dEMocracy', 'dimon', 'moDel')
-result2 = single_root_words('un', 'uNpoPulaR', 'UnDying', 'dying', 'jUngle')
+    
+result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
 
 print(result1)
 print(result2)
